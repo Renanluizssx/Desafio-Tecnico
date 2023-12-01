b077810b721117f53cb77f2a57e53b6247cb05b1
Renanluizssx <renanluizssx@gmail.com>
2023-12-01 15:33:19 -0300

    commitado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 function App() {
+  const [numero, setNumero] = useState(0);
+  const [soma, setSoma] = useState(0);
+  const somaTecla = () => {
+    let soma = 0;
+
+    for (let n = 1; n < numero; n++) {
+      if (n % 3 === 0 || n % 5 === 0) {
+        soma = soma + n;
+      }
+    }
+    return setSoma(soma);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-dark vh-100 text-light overflow-hidden">
+      <Row className="justify-content-center align-items-center h-100">
+        <Col xs={4}>
+          <div>
+            <h1 className="text-center">Bem vindo</h1>
+            <Form className="text-center">
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Control
+                  type="text"
+                  onChange={(tecla) => setNumero(tecla.target.value)}
+                  placeholder="Digite um número inteiro"
+                />
+              </Form.Group>
+              <Button variant="primary" onClick={() => somaTecla()}>
+                Gerar
+              </Button>
+            </Form>
+
+            <p className="text-center">{soma}</p>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
